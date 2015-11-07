000fc08e9f8f0e380dce8763ed1e30a1c9572438
cat_in_136 <cat.in.136+github@gmail.com>
2015-11-07 18:10:40 +0900

    fix degression bug that the tkgmap custom field breaks the issue page

--- app/helpers/tkgmap_helper.rb
+++ app/helpers/tkgmap_helper.rb
@@ -1,2 +1,7 @@
 module TkgmapHelper
+  def gmap_api_uri
+    uri = 'https://maps.google.com/maps/api/js?v=3&sensor=false'
+    uri << '&key=' << Setting.plugin_redmine_tkgmap['gmap_api_key'] if Setting.plugin_redmine_tkgmap['gmap_api_key'].present?
+    uri
+  end
 end
