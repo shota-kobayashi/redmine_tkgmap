1209f145c305a2785362f90cc2c1ee05c4296f66
cat_in_136 <cat.in.136+github@gmail.com>
2017-11-03 10:26:22 +0900

    let the plugin compatible with redmine 3.4.0
    This follows the functional change of IssueHelper#render_custom_fields_rows
    according to Redmine Patch#21705 <https://www.redmine.org/issues/21705>.

--- lib/issues_helper_patch.rb
+++ lib/issues_helper_patch.rb
@@ -2,9 +2,13 @@ require_dependency 'issues_helper'
 
 module IssuesHelperPatch
 	def self.included(base)
-		base.send(:include, InstanceMethods)
-		base.class_eval do
-			alias_method_chain :render_custom_fields_rows, :tkg
+		if Gem::Version.new([Redmine::VERSION::MAJOR, Redmine::VERSION::MINOR].join(".")) >= Gem::Version.new("3.4") # redmine >= 3.4
+			base.send(:prepend, InstanceMethods)
+		else
+			base.send(:include, InstanceMethods)
+			base.class_eval do
+				alias_method_chain :render_custom_fields_rows, :tkg
+			end
 		end
 	end
 
@@ -24,7 +28,7 @@ module IssuesHelperPatch
 					safe_join header_tags
 				end
 
-				s = ''
+				s = String.new
 				latLng = sv.split(",")
 				default_zoom = 'undefined'
 				default_zoom = Setting.plugin_redmine_tkgmap['default_zoom'].to_i if default_zoom.present?
@@ -43,6 +47,26 @@ module IssuesHelperPatch
 		end
 		private :show_tkgmap_value
 
+		if Gem::Version.new([Redmine::VERSION::MAJOR, Redmine::VERSION::MINOR].join(".")) >= Gem::Version.new("3.4") # redmine >= 3.4
+
+		def render_half_width_custom_fields_rows(issue)
+			values = issue.try(:visible_custom_field_values) || issue.custom_field_values
+			return if values.empty?
+			half = (values.size / 2.0).ceil
+			issue_fields_rows do |rows|
+				values.each_with_index do |value, i|
+					sv = (value.custom_field.field_format == Tkgmap::Identifier)? show_tkgmap_value(value) : show_value(value)
+
+					css = "cf_#{value.custom_field.id}".dup
+					css << ' tkgmap' if value.custom_field.field_format == Tkgmap::Identifier
+					m = (i < half ? :left : :right)
+					rows.send m, custom_field_name_tag(value.custom_field), sv, :class => css
+				end
+			end
+		end
+
+		else # redmine < 3.4
+
 		def render_custom_fields_rows_with_tkg(issue)
 			values = issue.try(:visible_custom_field_values) || issue.custom_field_values
 			return if values.empty?
@@ -58,7 +82,7 @@ module IssuesHelperPatch
 							sv = simple_format_without_paragraph(h(show_value(value)))
 						end
 
-						css = "cf_#{value.custom_field.id}"
+						css = "cf_#{value.custom_field.id}".dup
 						css << ' tkgmap' if value.custom_field.field_format == Tkgmap::Identifier
 						m = (i < half ? :left : :right)
 						rows.send m, custom_field_name_tag(value.custom_field), sv, :class => css
@@ -71,7 +95,7 @@ module IssuesHelperPatch
 					ordered_values << values[i]
 					ordered_values << values[i + half]
 				end 
-				s = "<tr>\n"
+				s = "<tr>\n".dup
 				n = 0 
 				ordered_values.compact.each do |value|
 					sv = ""
@@ -89,6 +113,8 @@ module IssuesHelperPatch
 				s.html_safe
 			end
 		end
+		end
+
 	end
 end
 
